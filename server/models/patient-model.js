3b3d4169e2a1b2737a567c771527a1e7b2e41ab4
Jasmine Bien-Aime <82053164+Jasminethecreator@users.noreply.github.com>
2022-02-19 23:03:14 -0500

    delete icuAdmin property

--- server/models/patient-model.js
+++ server/models/patient-model.js
@@ -8,34 +8,28 @@ const Patient = new Schema({
             patientId: {
                 type: Mixed,
                 required: true
-                },
-                
-         
+            },
+               
             age: {
                 type: Number,
                 required: false
-            },
+            },    
             
-          
             zipCode: {
                 type: String,
                 required: false
             },
             
+            
             latestBmi: {
                 type: Number,
                 required: false
             },
+            
+            
             latestWeight: {
                 type: Number,
                 required: false
-            },
-
-            
-
-            icuAdmin: {
-                type: Boolean,
-                required: true
             },
 
             
