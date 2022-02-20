c15296ccba1aae7333ba2adc0b67d32bde5e4a3e
Jasmine Bien-Aime <82053164+Jasminethecreator@users.noreply.github.com>
2022-02-19 21:50:40 -0500

    remove testName property

--- server/models/exam-model.js
+++ server/models/exam-model.js
@@ -4,11 +4,7 @@ const Schema = mongoose.Schema;
 //defining exam schemas maps to a MongoBD collection
 
 const Exam = new Schema ( {
-        
-        testName: {
-            type: String,
-            required: false
-        },
+       
         lastVisit: {
             type: String,
             required: false
